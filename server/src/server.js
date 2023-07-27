468b77c096aa72faecfdc2703e896a45cd3fa1ab
Gabriela Carniel <gabiicarniel@gmail.com>
2023-07-27 15:54:46 -0300

    env variables

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors')
+require('dotenv/config')
 
 const express = require("express")
 const routes = require("./routes")
@@ -33,5 +34,5 @@ app.use((error, req, res, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
