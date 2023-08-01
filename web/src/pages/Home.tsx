5dad4d58e90678221fd934eba5ed70b545afcda7
Gabriela Carniel <gabiicarniel@gmail.com>
2023-08-01 16:35:26 -0300

    added cafe test

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -67,6 +67,7 @@ export function Home() {
             <Section title="Refeições" data={products} categoryType='refeicoes' />
             <Section title="Sobremesas" data={products} categoryType='sobremesas' />
             <Section title="Bebidas" data={products} categoryType='bebidas' />
+            <Section title="Cafés" data={products} categoryType='cafe' />
           </>
       }
 
